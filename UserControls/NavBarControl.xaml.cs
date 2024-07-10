1da1524109e693da270106594a5db3378af48d97
Philippe Martin <philippeoliviermartin@gmail.com>
2024-07-10 08:35:09 -0400

    Added a navigation bar

---
+++ UserControls/NavBarControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using MangaReader.Pages;
+
+namespace MangaReader.UserControls
+{
+    public partial class NavBarControl : UserControl
+    {
+        public NavBarControl()
+        {
+
+            InitializeComponent();
+        }
+
+        private void BookClicked(object sender, MouseButtonEventArgs e)
+        {
+            var navigationService = NavigationService.GetNavigationService(this);
+            navigationService?.Navigate(new SelectionPage());
+        }
+
+        private void UserClicked(object sender, MouseButtonEventArgs e)
+        {
+            var navigationService = NavigationService.GetNavigationService(this);
+            navigationService?.Navigate(null);
+        }
+    }
+}
