1d87d51a09228475f0031dc42c3eef0255c1eb09
Philippe Martin <philippeoliviermartin@gmail.com>
2024-07-11 12:42:17 -0400

    added indication for json translation of manga object

--- Model/Manga.cs
+++ Model/Manga.cs
@@ -1,9 +1,13 @@
-﻿namespace MangaReader.Model
+﻿using Newtonsoft.Json;
+
+namespace MangaReader.Model
 {
     public class Manga
     {
         public string Id { get; set; }
         public string Title { get; set; }
+
+        [JsonProperty("available_chapters")]
         public int AvailableChapters { get; set; }
 
         public string Miniature
